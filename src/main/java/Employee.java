53cd18bb646677a3b90eba88184fcc1d303a5a46
Patryk Milkiewicz <g00397265@atu.com>
2022-12-07 11:48:04 +0000

    set up test cases for Employee name

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,2 +1,25 @@
 public class Employee {
+    private String name;
+    private String employeeNumber;
+
+    public Employee(String name, String employeeNumber) {
+        setName(name);
+        setEmployeeNumber(employeeNumber);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(String employeeNumber) {
+        employeeNumber = employeeNumber;
+    }
 }
