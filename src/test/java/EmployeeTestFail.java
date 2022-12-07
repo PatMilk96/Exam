febfe4f275492ba3b4553d22b37d2900f4a5123b
Patryk Milkiewicz <g00397265@atu.com>
2022-12-07 12:11:37 +0000

    added code to employeeGetName class

--- src/test/java/EmployeeTestFail.java
+++ src/test/java/EmployeeTestFail.java
@@ -1,17 +1,25 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTestFail {
+    @BeforeEach
+    void setUp() { Employee employee = new Employee("Patrick", "123456789101"); }
 
     @Test
     void employeeNumberFail() {
-        Employee employee = new Employee("Patrick", "1234567891011");
+        Employee employee = new Employee("Patrick", "1234567");
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, employee::getEmployeeNumber);
         assertEquals("Employee Number should be 12 digits long", exception.getMessage());
     }
 
     @Test
     void employeeNameFail() {
+    }
+
+    @AfterEach
+    void tearDown() {
     }
 }
